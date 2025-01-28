a2e997ec9d88dd30c7f00947296f392d84169348
Swapna M <sm2196@hw.ac.uk>
2025-01-28 23:00:48 +0400

    code

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -3,9 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import './App.css';
 import Home from './components/pages/Home';
-import Services from './components/pages/Services';
-import Products from './components/pages/Products';
-import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
@@ -13,9 +10,9 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/services" element={<Services />} />
+        {/* <Route path="/services" element={<Services />} />
         <Route path="/products" element={<Products />} />
-        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/sign-up" element={<SignUp />} /> */}
       </Routes>
     </Router>
   );
