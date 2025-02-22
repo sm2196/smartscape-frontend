7ab44091c4df6878825ab86e16a158381676d7d9
Abhijith Pattali <ap2092@hw.ac.uk>
2025-02-22 16:13:22 +0400

    changed all the contents of components to a new folder Abhijith

--- src/app/ap_app.jsx
+++ src/app/ap_app.jsx
@@ -1,20 +1,15 @@
 import React from "react";
 import "./ap_app.css"; // Import global styles
-import Navbar from "../components/navbar/ap_Navbar";
-//import { Sidebar } from '../../components/devices/Sidebar';
-import DeviceManagement from "../components/main/ap_DeviceManagement";
+import Navbar from "../Abhijith/navbar/ap_Navbar";
+import DeviceManagement from "../Abhijith/main/ap_DeviceManagement";
 
 export default function App() {
   return (
     <div className="ap_layout">
       {/* Navbar */}
-      {/* <div className="sidebar">
-        <Sidebar />
-      </div> */}
       <div className="navbar">
         <Navbar />
       </div>
-
       {/* Main Content */}
       <div className="ap_main-content">
         <DeviceManagement />
