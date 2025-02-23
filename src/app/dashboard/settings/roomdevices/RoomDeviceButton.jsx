3c29445b180b72fddb7abc8e49e9885542d5d317
Akbar Ali Salim Ali <as2241@hw.ac.uk>
2025-02-23 22:49:53 +0400

    fix roomdevice button logic

--- src/app/dashboard/settings/roomdevices/RoomDeviceButton.jsx
+++ src/app/dashboard/settings/roomdevices/RoomDeviceButton.jsx
@@ -1,5 +1,9 @@
-const Button = ({ children, className = "" }) => {
-  return <button className={`${className}`}>{children}</button>;
+const Button = ({ children, className = "", ...props }) => {
+  return (
+    <button className={`${className}`} {...props}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
