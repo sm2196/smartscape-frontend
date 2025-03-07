0c90a89030673bbb6ad7393bb0883e4946ace626
Rushaan78 <rs2095@hw.ac.uk>
2025-03-08 01:01:32 +0400

    added features

--- src/app/CustomerSupport.jsx
+++ src/app/CustomerSupport.jsx
@@ -1,23 +1,39 @@
 import React, { useState } from "react";
+import { FaVideo } from "react-icons/fa6";
+import { IoChatbubblesOutline } from "react-icons/io5";
 
 const FAQ = () => {
 
   return (
-    <div className="faq-container">
-      <div className="faq-header">
-        <h2>Customer Support</h2>
-        <p className="faq-subtitle">
-          Here are some answers to the most common questions we get asked
-        </p>
+   <div className="faq-container">
+         <div className="faq-header">
+           <h2>Customer Support</h2>
+           <p className="faq-subtitle">
+             Here are some answers to the most common questions we get asked
+           </p>
 
-        {/* Search bar */}
-        <input
-          type="text"
-          className="faq-search-bar"
-          placeholder="Search for a question..."
-        />
-      </div>
+           {/* Search bar */}
+           <input
+             type="text"
+             className="faq-search-bar"
+             placeholder="Search for a question..."
+           />
+         </div>
 
+
+         <button className="videoBtn" >
+           <span className="IconContainer">
+             <FaVideo className="icon" />
+           </span>
+           <p className="text">Watch Tutorial</p>
+         </button>
+
+         <div className="wrapper">
+         <div className="icon live-chat" >
+           <IoChatbubblesOutline size={30} color="#fff" />
+           <div className="tooltip">Chat</div>
+         </div>
+       </div>
 
   </div>)
 };
